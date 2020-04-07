e71ff52ce7338b89307cf45002f6bf9bb913a844
Alexander Berndt <github@alberndt.de>
2020-04-08 00:44:32 +0200

    First trial of ProcessingContext

--- core/src/main/java/com/alexanderberndt/appintegration/engine/pipeline/api/ProcessingContext.java
+++ core/src/main/java/com/alexanderberndt/appintegration/engine/pipeline/api/ProcessingContext.java
@@ -1,4 +1,16 @@
 package com.alexanderberndt.appintegration.engine.pipeline.api;
 
+import java.util.Map;
+
 public class ProcessingContext {
+
+    private Map<String, Object> pipelineFilterConfiguration;
+
+    public Map<String, Object> getPipelineFilterConfiguration() {
+        return pipelineFilterConfiguration;
+    }
+
+    public void setPipelineFilterConfiguration(Map<String, Object> pipelineFilterConfiguration) {
+        this.pipelineFilterConfiguration = pipelineFilterConfiguration;
+    }
 }
--- core/src/test/java/com/alexanderberndt/appintegration/engine/pipeline/ProcessingPipelineBuilderTest.java
+++ core/src/test/java/com/alexanderberndt/appintegration/engine/pipeline/ProcessingPipelineBuilderTest.java
@@ -1,8 +1,11 @@
 package com.alexanderberndt.appintegration.engine.pipeline;
 
+import com.alexanderberndt.appintegration.api.ContextProvider;
 import com.alexanderberndt.appintegration.engine.converter.StreamToReaderConverter;
+import com.alexanderberndt.appintegration.engine.filter.RegexReplaceFilter;
 import com.alexanderberndt.appintegration.engine.filter.SearchReplaceFilter;
 import com.alexanderberndt.appintegration.engine.filter.TextSnippetExtractor;
+import com.alexanderberndt.appintegration.engine.pipeline.api.ProcessingContext;
 import com.alexanderberndt.appintegration.engine.validators.FileSizeValidator;
 import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Test;
@@ -11,6 +14,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.Reader;
 import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,12 +26,24 @@ class ProcessingPipelineBuilderTest {
         InputStream input = ClassLoader.getSystemResourceAsStream("simple-app1/server/resources/to-be-filtered1.txt");
         assertNotNull(input);
 
+        ProcessingContext context = new ProcessingContext();
+
+
         ProcessingPipelineBuilder<InputStream> builder = new ProcessingPipelineBuilder<>(input);
         builder.appendFilter(null, new FileSizeValidator());
         builder.appendFilter(null, new FileSizeValidator());
         builder.appendFilter(null, new StreamToReaderConverter());
         builder.appendFilter(null, new TextSnippetExtractor());
         builder.appendFilter(null, new SearchReplaceFilter());
+
+
+        Map<String, Object> config = new HashMap<>();
+        config.put("regex", "World");
+        config.put("replacement", "Earth");
+        context.setPipelineFilterConfiguration(config);
+
+
+        builder.appendFilter(context, new RegexReplaceFilter());
 
         StringWriter output = new StringWriter();
 
