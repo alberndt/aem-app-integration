9eef57f84f98847891e0230c2e0508e4c91ae1ea
Alexander Berndt <github@alberndt.de>
2020-09-15 19:56:12 +0200

    fixed SonarQube findings

--- server/aem/bundle/src/main/java/com/alexanderberndt/appintegration/aem/servlets/AemAppIntegrationProxyServlet.java
+++ server/aem/bundle/src/main/java/com/alexanderberndt/appintegration/aem/servlets/AemAppIntegrationProxyServlet.java
@@ -1,9 +1,12 @@
 package com.alexanderberndt.appintegration.aem.servlets;
 
 import com.alexanderberndt.appintegration.aem.engine.AemAppIntegrationEngine;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.Servlet;
 import javax.servlet.http.HttpServlet;
@@ -11,6 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.invoke.MethodHandles;
 
 
 @Component(
@@ -24,24 +28,27 @@ public class AemAppIntegrationProxyServlet extends HttpServlet {
 
     private static final long serialVersionUID = -1812227983368047424L;
 
+    private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
     private final AemAppIntegrationEngine integrationEngine;
 
+    @Activate
     public AemAppIntegrationProxyServlet(@Reference AemAppIntegrationEngine integrationEngine) {
         this.integrationEngine = integrationEngine;
     }
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
         response.setContentType("text/plain");
         response.setCharacterEncoding("utf-8");
-
-        PrintWriter out = response.getWriter();
-
-        out.println("Hello World!");
-
+        try {
+            PrintWriter out = response.getWriter();
+            out.println("Hello World!");
+            out.println(integrationEngine);
+        } catch (IOException e) {
+            LOG.error("Cannot get writer", e);
+        }
     }
-
-
 }
 
 
