4f31e0f9bf67d0ff31bd52b7ed24c21f4ad496bc
Alexander Berndt <github@alberndt.de>
2020-09-18 21:53:02 +0200

    fix AemGlobalContext

--- server/aem/bundle/src/main/java/com/alexanderberndt/appintegration/aem/engine/AemGlobalContext.java
+++ server/aem/bundle/src/main/java/com/alexanderberndt/appintegration/aem/engine/AemGlobalContext.java
@@ -9,20 +9,31 @@ import com.alexanderberndt.appintegration.pipeline.configuration.Ranking;
 import com.alexanderberndt.appintegration.pipeline.context.GlobalContext;
 import com.alexanderberndt.appintegration.pipeline.context.TaskContext;
 import com.alexanderberndt.appintegration.utils.DataMap;
+import org.apache.sling.api.resource.ResourceResolver;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 public class AemGlobalContext extends GlobalContext {
 
-    protected AemGlobalContext(@Nonnull final LogAppender logAppender, @Nonnull VerifiedApplication verifiedApplication, @Nonnull AppIntegrationEngine<?, ?> engine) {
+    @Nonnull
+    private final ResourceResolver resourceResolver;
+
+
+    protected AemGlobalContext(@Nonnull final LogAppender logAppender, @Nonnull VerifiedApplication verifiedApplication, @Nonnull AppIntegrationEngine<?, ?> engine, @Nonnull final ResourceResolver resourceResolver) {
         super(logAppender, verifiedApplication, engine);
+        this.resourceResolver = resourceResolver;
     }
 
     @Nonnull
     @Override
     public TaskContext createTaskContext(@Nonnull TaskLogger taskLogger, @Nonnull Ranking rank, @Nonnull String taskId, @Nonnull ExternalResourceType resourceType, @Nullable DataMap executionDataMap) {
         return new AemTaskContext(this, taskLogger, rank, taskId, resourceType, executionDataMap);
+    }
+
+    @Nonnull
+    public ResourceResolver getResourceResolver() {
+        return resourceResolver;
     }
 
 }
