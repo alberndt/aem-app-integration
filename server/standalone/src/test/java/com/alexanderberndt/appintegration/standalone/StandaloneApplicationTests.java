190802f7dc7d6f5be8e299e47bc53300a62e5132
Alexander Berndt <github@alberndt.de>
2020-09-23 22:04:53 +0200

    clean-up SonarQube findings, removing old useless JUnit tests

--- server/standalone/src/test/java/com/alexanderberndt/appintegration/standalone/StandaloneApplicationTests.java
+++ server/standalone/src/test/java/com/alexanderberndt/appintegration/standalone/StandaloneApplicationTests.java
@@ -1,13 +1,24 @@
 package com.alexanderberndt.appintegration.standalone;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class StandaloneApplicationTests {
 
+	@Autowired
+	StandaloneApplication standaloneApplication;
+
 	@Test
 	void contextLoads() {
+		assertNotNull(standaloneApplication);
+
 	}
 
 }
